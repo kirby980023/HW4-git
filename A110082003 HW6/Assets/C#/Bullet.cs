004189bee3dd0be9b39311388ca949044e9c6e0b
LAPTOP-93CQVC3A\POKEM <kirby980023@gmail.com>
2022-10-21 18:51:25 +0800

    修正程式碼

--- A110082003 HW6/Assets/C#/Bullet.cs
+++ A110082003 HW6/Assets/C#/Bullet.cs
@@ -12,41 +12,21 @@ public class Bullet : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * 100;
-
-        // IEnumerator mc = MyCoroutine();
-
-        // StartCoroutine(mc);
-        // StopCoroutine(mc);
     }
 
     void Update()
     {
-         lifeTime += Time.deltaTime;
+        lifeTime += Time.deltaTime;
         
         if (lifeTime > 0.3f)
         {
             Destroy(gameObject);
         }
     }
-// IEnumerator MyCoroutine()
-// {
-//     StartCoroutine(mc);
-//     // 直接執行這裡...
 
-//     // 停兩秒
-//     yield return new WaitForSeconds(2);
-
-//     // 兩秒之後才執行這裡
-//     StopCoroutine(mc);
-// }
-    //  IEnumerator DestroySelf()
-    // {
-    //     yield return new WaitForSeconds(2);
-    //     gameObject.SetActive(false);
-    //     Destroy(gameObject);
-    // }
     private void OnCollisionEnter(Collision other)
     {
+
           if (other.gameObject.tag == "Monster")
         {
             other.gameObject.SetActive(false);
